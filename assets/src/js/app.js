68f329bcef7b0ca2c3e2ea2ce32d779a3ad390af
Francis Hogue <francis.hogue@twistimage.com>
2014-11-09 22:03:55 -0500

    Replace tabs for spaces

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -3,76 +3,76 @@
 'use strict';
 
 /*
-	Markup-based means of executing JavaScript on page load (using jQuery)
+  Markup-based means of executing JavaScript on page load (using jQuery)
 
-	How to use:
+  How to use:
 
-	Add this to your <body>
-	<body id="myapp" data-controller="module" data-action="action1 action2">
+  Add this to your <body>
+  <body id="myapp" data-controller="module" data-action="action1 action2">
 
-	Replace "myapp" to match what you put as 
-	the body ID (use something meaningful and short for the project)
+  Replace "myapp" to match what you put as 
+  the body ID (use something meaningful and short for the project)
 
-	Replace "MYAPP" to match what you put as the body ID (but capitalized)
+  Replace "MYAPP" to match what you put as the body ID (but capitalized)
 */
 
 (function($, window, document, undefined) {
 
-	var MYAPP = window.MYAPP = window.MYAPP || {};
+  var MYAPP = window.MYAPP = window.MYAPP || {};
 
-	MYAPP.util = {
-		exec: function(controller, action) {
-			var ns = MYAPP,
-			act = (action === undefined) ? 'init' : action;
+  MYAPP.util = {
+    exec: function(controller, action) {
+      var ns = MYAPP,
+      act = (action === undefined) ? 'init' : action;
 
-			if (controller !== '' && ns[controller] && typeof ns[controller][act] === 'function') {
-				ns[controller][act]();
-			}
-		},
-		init: function() {
-			var el = document.getElementById('myapp'), // Update the body ID here
-			controller = el.getAttribute('data-controller'),
-			actions = el.getAttribute('data-action');
+      if (controller !== '' && ns[controller] && typeof ns[controller][act] === 'function') {
+        ns[controller][act]();
+      }
+    },
+    init: function() {
+      var el = document.getElementById('myapp'), // Update the body ID here
+      controller = el.getAttribute('data-controller'),
+      actions = el.getAttribute('data-action');
 
-			MYAPP.util.exec('common');
-			MYAPP.util.exec(controller);
+      MYAPP.util.exec('common');
+      MYAPP.util.exec(controller);
 
-			// do all the actions too.
-			$.each(actions.split(/\s+/), function(i, action){
-				MYAPP.util.exec(controller, action);
-			});
-		}
-	};
-
-
-	// Common to the whole app/site
-	MYAPP.common = (function() {
-		var moduleVariable = '';
-		var privateFunction = function() {};
-		var init = function() {
-			console.log('common.init');
-		};
-
-		return {
-			init: init
-		};
-	})();
+      // do all the actions too.
+      $.each(actions.split(/\s+/), function(i, action){
+        MYAPP.util.exec(controller, action);
+      });
+    }
+  };
 
 
-	// Example module for the homepage
-	MYAPP.module = (function() {
-		var moduleVariable = '';
-		var privateFunction = function() {};
-		var init = function() { console.log('module.init'); };
-		var action1 = function() { console.log('module.action1'); };
-		var action2 = function() { console.log('module.action2'); };
+  // Common to the whole app/site
+  MYAPP.common = (function() {
+    var moduleVariable = '';
+    var privateFunction = function() {};
+    var init = function() {
+      console.log('common.init');
+    };
 
-		return {
-			init: init,
-			action1: action1,
-			action2: action2
-		};
-	})();
+    return {
+      init: init
+    };
+  })();
+
+
+  // Example module for the homepage
+  MYAPP.module = (function() {
+    var moduleVariable = '';
+    var privateFunction = function() {};
+    var init = function() { console.log('module.init'); };
+    var action1 = function() { console.log('module.action1'); };
+    var action2 = function() { console.log('module.action2'); };
+
+    return {
+      init: init,
+      action1: action1,
+      action2: action2
+    };
+  })();
 
 
 })(jQuery, window, document);
--- assets/src/js/helpers.js
+++ assets/src/js/helpers.js
@@ -2,25 +2,25 @@
 
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-	var method;
-	var noop = function () {};
-	var methods = [
-		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-		'timeStamp', 'trace', 'warn'
-	];
-	var length = methods.length;
-	var console = (window.console = window.console || {});
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
 
-	while (length--) {
-		method = methods[length];
+  while (length--) {
+    method = methods[length];
 
-		// Only stub undefined methods.
-		if (!console[method]) {
-			console[method] = noop;
-		}
-	}
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
 }());
 
 
@@ -28,12 +28,12 @@
 
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
-	return  window.requestAnimationFrame       ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame    ||
-	function( callback ){
-		window.setTimeout(callback, 1000 / 60);
-	};
+  return  window.requestAnimationFrame       ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame    ||
+  function( callback ){
+    window.setTimeout(callback, 1000 / 60);
+  };
 })();
 
 
@@ -54,25 +54,25 @@ window.requestAnimFrame = (function(){
  * @param {int} delay The delay in milliseconds
  */
 window.requestTimeout = function(fn, delay) {
-	if( !window.requestAnimationFrame       &&
-		!window.webkitRequestAnimationFrame &&
-		!window.mozRequestAnimationFrame    &&
-		!window.oRequestAnimationFrame      &&
-		!window.msRequestAnimationFrame)
-			return window.setTimeout(fn, delay);
+  if( !window.requestAnimationFrame       &&
+    !window.webkitRequestAnimationFrame &&
+    !window.mozRequestAnimationFrame    &&
+    !window.oRequestAnimationFrame      &&
+    !window.msRequestAnimationFrame)
+      return window.setTimeout(fn, delay);
 
-	var start = new Date().getTime(),
-		handle = new Object();
+  var start = new Date().getTime(),
+    handle = new Object();
 
-	function loop(){
-		var current = new Date().getTime(),
-		delta = current - start;
+  function loop(){
+    var current = new Date().getTime(),
+    delta = current - start;
 
-		delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
-	};
+    delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
+  };
 
-	handle.value = requestAnimFrame(loop);
-	return handle;
+  handle.value = requestAnimFrame(loop);
+  return handle;
 };
 
 /**
@@ -80,12 +80,12 @@ window.requestTimeout = function(fn, delay) {
  * @param {int|object} fn The callback function
  */
 window.clearRequestTimeout = function(handle) {
-	window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
-	window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value)   :
-	window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
-	window.oCancelRequestAnimationFrame ? window.oCancelRequestAnimationFrame(handle.value) :
-	window.msCancelRequestAnimationFrame ? msCancelRequestAnimationFrame(handle.value) :
-	clearTimeout(handle);
+  window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
+  window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value)   :
+  window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
+  window.oCancelRequestAnimationFrame ? window.oCancelRequestAnimationFrame(handle.value) :
+  window.msCancelRequestAnimationFrame ? msCancelRequestAnimationFrame(handle.value) :
+  clearTimeout(handle);
 };
 
 
@@ -95,44 +95,44 @@ window.clearRequestTimeout = function(handle) {
  * @param {int} delay The delay in milliseconds
  */
 window.requestInterval = function(fn, delay) {
-	if( !window.requestAnimationFrame       &&
-		!window.webkitRequestAnimationFrame &&
-		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
-		!window.oRequestAnimationFrame      &&
-		!window.msRequestAnimationFrame)
-			return window.setInterval(fn, delay);
+  if( !window.requestAnimationFrame       &&
+    !window.webkitRequestAnimationFrame &&
+    !(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+    !window.oRequestAnimationFrame      &&
+    !window.msRequestAnimationFrame)
+      return window.setInterval(fn, delay);
 
-	var start = new Date().getTime(),
-		handle = new Object();
+  var start = new Date().getTime(),
+    handle = new Object();
 
-	function loop() {
-		var current = new Date().getTime(),
-			delta = current - start;
+  function loop() {
+    var current = new Date().getTime(),
+      delta = current - start;
 
-		if(delta >= delay) {
-			fn.call();
-			start = new Date().getTime();
-		}
+    if(delta >= delay) {
+      fn.call();
+      start = new Date().getTime();
+    }
 
-		handle.value = requestAnimFrame(loop);
-	};
+    handle.value = requestAnimFrame(loop);
+  };
 
-	handle.value = requestAnimFrame(loop);
-	return handle;
+  handle.value = requestAnimFrame(loop);
+  return handle;
 }
 
 /**
  * Behaves the same as clearInterval except uses cancelRequestAnimationFrame() where possible for better performance
  * @param {int|object} fn The callback function
  */
-	window.clearRequestInterval = function(handle) {
-	window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
-	window.webkitCancelAnimationFrame ? window.webkitCancelAnimationFrame(handle.value) :
-	window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value) :
-	window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
-	window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
-	window.msCancelRequestAnimationFrame ? window.msCancelRequestAnimationFrame(handle.value) :
-	clearInterval(handle);
+  window.clearRequestInterval = function(handle) {
+  window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
+  window.webkitCancelAnimationFrame ? window.webkitCancelAnimationFrame(handle.value) :
+  window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value) :
+  window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
+  window.oCancelRequestAnimationFrame ? window.oCancelRequestAnimationFrame(handle.value) :
+  window.msCancelRequestAnimationFrame ? window.msCancelRequestAnimationFrame(handle.value) :
+  clearInterval(handle);
 };
 
 /* jshint ignore:end */
@@ -140,35 +140,35 @@ window.requestInterval = function(fn, delay) {
 
 
 (function($, window, undefined) {
-	'use strict';
+  'use strict';
 
-	var TITOOLBOX = window.TITOOLBOX = window.TITOOLBOX || {};
+  var TITOOLBOX = window.TITOOLBOX = window.TITOOLBOX || {};
 
-	// Detect whether device supports orientationchange event, otherwise fall back to
-	// the resize event.
-	TITOOLBOX.orientationEvent = function() {
-		var supportsOrientationChange = 'onorientationchange' in window;
-		return supportsOrientationChange ? 'orientationchange' : 'resize';
-	};
+  // Detect whether device supports orientationchange event, otherwise fall back to
+  // the resize event.
+  TITOOLBOX.orientationEvent = function() {
+    var supportsOrientationChange = 'onorientationchange' in window;
+    return supportsOrientationChange ? 'orientationchange' : 'resize';
+  };
 
-	// Transition event name
-	// $(selector).one(TITOOLBOX.transEndEventName(), function(e) {});
-	TITOOLBOX.transEndEventName = function() {
-		var transEndEventNames = {
-				'WebkitTransition' : 'webkitTransitionEnd',
-				'MozTransition'    : 'transitionend',
-				'OTransition'      : 'oTransitionEnd otransitionend',
-				'msTransition'     : 'MSTransitionEnd',
-				'transition'       : 'transitionend'
-			},
-			transEndEventName = transEndEventNames[Modernizr.prefixed('transition')];
+  // Transition event name
+  // $(selector).one(TITOOLBOX.transEndEventName(), function(e) {});
+  TITOOLBOX.transEndEventName = function() {
+    var transEndEventNames = {
+        'WebkitTransition' : 'webkitTransitionEnd',
+        'MozTransition'    : 'transitionend',
+        'OTransition'      : 'oTransitionEnd otransitionend',
+        'msTransition'     : 'MSTransitionEnd',
+        'transition'       : 'transitionend'
+      },
+      transEndEventName = transEndEventNames[Modernizr.prefixed('transition')];
 
-		// Android 4.1.2 hotfix (Samsung S3)
-		if (navigator.userAgent.toLowerCase().indexOf('android 4.1') !== -1) {
-			transEndEventName = 'webkitTransitionEnd';
-		}
+    // Android 4.1.2 hotfix (Samsung S3)
+    if (navigator.userAgent.toLowerCase().indexOf('android 4.1') !== -1) {
+      transEndEventName = 'webkitTransitionEnd';
+    }
 
-		return transEndEventName;
-	};
+    return transEndEventName;
+  };
 
 })(jQuery, window);
